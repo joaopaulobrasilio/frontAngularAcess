3f8a997a76213c63ee1e85a2257cbdc73b067e80
joaopaulo <joaopaulobrasilio947@gmail.com>
2023-11-28 22:48:03 -0300

    criando novas func

--- app.module.ts
+++ app.module.ts
@@ -8,17 +8,18 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AcessoModule } from './components/acesso/acesso.module';
-import { NewAcessosComponent } from './components/new-acessos/new-acessos.component';
+
 import { SpinnerComponent } from './components/spinner/spinner.component';
 import { LoadingInterceptor } from './interceptors/loading.interceptor';
 import { NewacessoModule } from './components/new-acessos/newacesso.module';
-import { LoginComponent } from './components/login/login.component';
+
 import { LoginModule } from './components/login/login.module';
 import { TokenInterceptor } from './token.interceptor';
 
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { EditarAcessosComponent } from './components/editar-acessos/editar-acessos.component';
+
 import { EditarAcessoModule } from './components/editar-acessos/editar-acesso.module';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { SharedModule } from './shared/shared.module';
 
 
 
@@ -26,7 +27,8 @@ import { EditarAcessoModule } from './components/editar-acessos/editar-acesso.mo
 @NgModule({
   declarations: [
     AppComponent,
-    SpinnerComponent,
+    SpinnerComponent
+
 
 
 
@@ -38,8 +40,9 @@ import { EditarAcessoModule } from './components/editar-acessos/editar-acesso.mo
     BrowserAnimationsModule,
     AcessoModule,
     NewacessoModule,
-    LoginModule,
-    EditarAcessoModule
+    EditarAcessoModule,
+    ModalModule.forRoot(),
+    SharedModule
 
 
 
